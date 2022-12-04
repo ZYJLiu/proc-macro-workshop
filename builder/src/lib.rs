69af563aabfe25bea4f83407fe44ce6e5c8175f9
john <johnzhengyuliu@gmail.com>
2022-12-03 19:24:43 -0600

    builder 05

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,74 @@
+extern crate proc_macro;
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = parse_macro_input!(input as DeriveInput);
+    let name = &ast.ident;
+    let bname = format!("{}Builder", name);
+    let bident = syn::Ident::new(&bname, name.span());
 
-    unimplemented!()
+    let fields = if let syn::Data::Struct(syn::DataStruct {
+        fields: syn::Fields::Named(syn::FieldsNamed { ref named, .. }),
+        ..
+    }) = ast.data
+    {
+        named
+    } else {
+        unimplemented!();
+    };
+
+    let optionized = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        quote! { #name: std::option::Option<#ty> }
+    });
+
+    let methods = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        quote! {
+            pub fn #name(&mut self, #name: #ty) -> &mut Self {
+                self.#name = Some(#name);
+                self
+            }
+        }
+    });
+
+    let build_fields = fields.iter().map(|f| {
+        let name = &f.ident;
+        quote! {
+            #name: self.#name.clone().ok_or(concat!(stringify!(#name), " is not set"))?
+        }
+    });
+
+    let build_empty = fields.iter().map(|f| {
+        let name = &f.ident;
+        quote! {#name: None}
+    });
+
+    let expanded = quote! {
+        struct #bident {
+            #(#optionized,)*
+        }
+        impl #bident {
+             #(#methods)*
+            pub fn build(&self) -> Result<#name, Box<dyn std::error::Error>> {
+                Ok(#name {
+                    #(#build_fields,)*
+                })
+            }
+        }
+        impl #name {
+            fn builder() -> #bident {
+                #bident {
+                     #(#build_empty,)*
+                }
+            }
+        }
+    };
+
+    expanded.into()
 }
--- main.rs
+++ main.rs
@@ -5,5 +5,14 @@
 //
 // To run the code:
 //     $ cargo run
+use derive_builder::Builder;
+
+#[derive(Builder)]
+pub struct Command {
+    executable: String,
+    args: Vec<String>,
+    env: Vec<String>,
+    current_dir: String,
+}
 
 fn main() {}

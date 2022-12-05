fc4002321d2fc04c200d286def26b303565630cd
john <johnzhengyuliu@gmail.com>
2022-12-04 20:08:05 -0600

    seq 01

--- main.rs
+++ main.rs
@@ -1,20 +1,7 @@
-// Write code here.
-//
-// To see what the code looks like after macro expansion:
-//     $ cargo expand
-//
-// To run the code:
-//     $ cargo run
-use derive_builder::Builder;
+use seq::seq;
 
-#[derive(Builder)]
-pub struct Command {
-    executable: String,
-    #[builder(each = "arg")]
-    args: Vec<String>,
-    #[builder(each = "env")]
-    env: Vec<String>,
-    current_dir: Option<String>,
-}
+seq!(N in 0..8 {
+    // nothing
+});
 
 fn main() {}
--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,8 +1,27 @@
 use proc_macro::TokenStream;
+use syn::parse::{Parse, ParseStream};
+use syn::{parse_macro_input, Result, Token};
+
+#[derive(Debug)]
+struct SeqMacroInput {/* ... */}
+
+impl Parse for SeqMacroInput {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let var = syn::Ident::parse(input)?;
+        let _in = <Token![in]>::parse(input)?;
+        let from = syn::Lit::parse(input)?;
+        let _dots = <Token![..]>::parse(input)?;
+        let to = syn::Lit::parse(input)?;
+        let body = syn::Block::parse(input)?;
+        eprintln!("{:?} {:?} {:?} {:?} {:?}", var, _in, from, _dots, to);
+        eprintln!("{:?}", body);
+        Ok(SeqMacroInput {})
+    }
+}
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let input = parse_macro_input!(input as SeqMacroInput);
+    println!("{:?}", input);
+    TokenStream::new()
 }

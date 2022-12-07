2be03a51cda3fcd307de499ad81f74af72f554ca
john <johnzhengyuliu@gmail.com>
2022-12-07 10:29:12 -0600

    builder 03

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -16,6 +16,25 @@ pub fn derive(input: TokenStream) -> TokenStream {
             env: Option<Vec<String>>,
             current_dir: Option<String>,
         }
+        impl #bident {
+            pub fn executable(&mut self, executable: String) -> &mut Self {
+                self.executable = Some(executable);
+                self
+            }
+            pub fn args(&mut self, args: Vec<String>) -> &mut Self {
+                self.args = Some(args);
+                self
+            }
+            pub fn env(&mut self, env: Vec<String>) -> &mut Self {
+                self.env = Some(env);
+                self
+            }
+            pub fn current_dir(&mut self, current_dir: String) -> &mut Self {
+                self.current_dir = Some(current_dir);
+                self
+            }
+        }
+
         impl #name {
             fn builder() -> #bident {
                 #bident {
